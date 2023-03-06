2668081f927f6ca5c0ec1c9b4692c669e74ff4a4
IamArshadAli <arshadalikaldane@gmail.com>
2023-03-06 23:52:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const track = document.getElementById("image-track");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = e => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+    
+    const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+
+        Math.min(nextPercentage, 0);
+        Math.max(nextPercentage, -100);
+    
+    track.dataset.percentage = nextPercentage;
+
+    // track.style.transform = `translateX(${nextPercentage}%)`;
+    track.animate({
+        transform: `translateX(${nextPercentage}%)`
+    }, {duration: 1200, fill: 'forwards'});
+
+    for(const image of track.getElementsByClassName("image")) {
+        // image.style.objectPosition = `${nextPercentage + 100} 50%`
+        image.animate({
+            objectPosition: `${100 + nextPercentage}% center`
+        }, {duration: 1200, fill: 'forwards'});
+    }
+}
